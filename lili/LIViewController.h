3b88a1a3082d769f69c951bc32d08ff1e5787c3a
i0xbean <i0xbean@gmail.com>
2014-04-26 09:59:17 +0800

    Initial Commit

---
+++ lili/LIViewController.h
@@ -0,0 +1,13 @@
+//
+//  LIViewController.h
+//  lili
+//
+//  Created by Ming Z. on 14-4-26.
+//  Copyright (c) 2014年 i0xbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LIViewController : UIViewController
+
+@end
---
+++ lili/LIViewController.m
@@ -0,0 +1,29 @@
+//
+//  LIViewController.m
+//  lili
+//
+//  Created by Ming Z. on 14-4-26.
+//  Copyright (c) 2014年 i0xbean. All rights reserved.
+//
+
+#import "LIViewController.h"
+
+@interface LIViewController ()
+
+@end
+
+@implementation LIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
